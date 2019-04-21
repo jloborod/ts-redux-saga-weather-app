b0d6bd20d891867deedd0442876e5dea7dd22d30
Joaquín Lobo Rodríguez <jloborod@gmail.com>
2019-04-21 14:43:39 +0100

    moved components into component folders and cleanup assets and styles from cra

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import './index.css';
-import App from './App';
+import App from './components/App/App';
 
 ReactDOM.render(
   <Provider store={store}>
