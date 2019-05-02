60cb58b7540c0485fdadda8546d258917051a6fc
Joaquín Lobo Rodríguez <jloborod@gmail.com>
2019-05-02 02:14:39 +0100

    added enzyme-to-json and serializer to setupTest.js to have more readable snapshots

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,9 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { createSerializer } from 'enzyme-to-json';
+
+// Set the default serializer for Jest to be enzyme-to-json
+// This produces an easier to read serialized format
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
 
 Enzyme.configure({ adapter: new Adapter() });
