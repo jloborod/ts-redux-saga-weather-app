7da833364bdd11261622afd268aa0ce158c4e970
Joaquín Lobo Rodríguez <jloborod@gmail.com>
2019-05-02 23:36:56 +0100

    added back index.css on src

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import './index.css';
+
 import store from './store';
 import App from './components/App';
 
